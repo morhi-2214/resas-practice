b55931c36132607f4f2b6ba3d0158783c389b580
h.mori <h.mori@sonicmoov.com>
2022-05-10 20:06:35 +0900

    feat: 都道府県名のcheckboxを表示

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,39 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
-import logo from "./logo.svg";
-
+import { Checkbox } from "./components/Checkbox";
+import { PopulationRepository } from "./repositories/populationRepository";
+import { RepositoryFactory } from "./repositories/repositoryFactory";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const { getPrefectures, getPopulation } = RepositoryFactory.get(
+    "population"
+  ) as PopulationRepository;
+
+  const [prefectures, setPrefectures] = useState<any>([]);
+  const [populations, setPopulation] = useState<any>([]);
+
+  useEffect(() => {
+    getPrefectures()
+      .then((res) => {
+        setPrefectures(res.result);
+      })
+      .catch((err) => console.log("error", err));
+
+    getPopulation()
+      .then((res) => {
+        setPopulation(res.result);
+      })
+      .catch((err) => console.log("error", err));
+  }, []);
+
+  // console.log(prefectures);
+  // console.log(populations);
 
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {" | "}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
+        <Checkbox items={prefectures} />
       </header>
     </div>
   );
